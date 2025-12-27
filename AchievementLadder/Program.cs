b2beb52ec713ec1b9ec2af53b5ccdd60afa6f6c5
HunorTotBagi <hunortotbagi@gmail.com>
2025-12-27 16:41:50 +0100

    feat: setup docker database and environment

--- AchievementLadder/Program.cs
+++ AchievementLadder/Program.cs
@@ -5,28 +5,23 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
-// Configure EF Core with Postgres
-builder.Services.AddDbContext<AchievementContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AchievementContext>(
+    options => options.UseNpgsql(builder.Configuration.GetConnectionString("Database")));
 
-// register app services
 builder.Services.AddScoped<ILadderRepository, LadderRepository>();
 builder.Services.AddScoped<ILadderService, LadderService>();
 
 var app = builder.Build();
 
-// Ensure database is created
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AchievementContext>();
-    db.Database.EnsureCreated();
-}
+//using (var scope = app.Services.CreateScope())
+//{
+//    var db = scope.ServiceProvider.GetRequiredService<AchievementContext>();
+//    db.Database.EnsureCreated();
+//}
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
