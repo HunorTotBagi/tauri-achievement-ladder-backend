3dc254bcf51edf431c63f2c6cd8d34cacc74650a
HunorTotBagi <hunortotbagi@gmail.com>
2026-01-04 09:25:18 +0100

    feat: add playerClass filtering to controller

--- AchievementLadder/Controllers/LadderController.cs
+++ AchievementLadder/Controllers/LadderController.cs
@@ -15,16 +15,16 @@ namespace AchievementLadder.Controllers
         }
 
         [HttpGet("sorted/achievements")]
-        public async Task<IActionResult> GetSortedByAchievements([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 500, [FromQuery] string? realm = null, [FromQuery] string? faction = null, CancellationToken cancellationToken = default)
+        public async Task<IActionResult> GetSortedByAchievements([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 500, [FromQuery] string? realm = null, [FromQuery] string? faction = null, [FromQuery] int? playerClass = null, CancellationToken cancellationToken = default)
         {
-            var result = await playerService.GetSortedByAchievements(pageNumber, pageSize, realm, faction, cancellationToken);
+            var result = await playerService.GetSortedByAchievements(pageNumber, pageSize, realm, faction, playerClass, cancellationToken);
             return Ok(result);
         }
 
         [HttpGet("sorted/honorableKills")]
-        public async Task<IActionResult> GetSortedByHonorableKills([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 500, [FromQuery] string? realm = null, [FromQuery] string? faction = null, CancellationToken cancellationToken = default)
+        public async Task<IActionResult> GetSortedByHonorableKills([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 500, [FromQuery] string? realm = null, [FromQuery] string? faction = null, [FromQuery] int? playerClass = null, CancellationToken cancellationToken = default)
         {
-            var result = await playerService.GetSortedByHonorableKills(pageNumber, pageSize, realm, faction, cancellationToken);
+            var result = await playerService.GetSortedByHonorableKills(pageNumber, pageSize, realm, faction, playerClass, cancellationToken);
             return Ok(result);
         }
     }
